2da8f50b84d068a9bf3220d5d034d82eb28c554c
pankaj7464 <pkumar44676@gamil.com>
2021-12-10 11:49:39 +0530

    form change stage

---
+++ app/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm
+from django.contrib.auth.models import User
+from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib.auth import password_validation
+
+
+class CustomerResistrationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label='password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}))
+    email = forms.CharField(label='Email', required=True, widget=forms.EmailInput(
+        attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        labels = {'email': 'Email'}
+        widgets = {'username': forms.TextInput(
+            attrs={'class': 'form-control'})}
+
+
+class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(
+        attrs={'autofocus': True, 'class': 'form-control'}))
+    password = forms.CharField(label=_('password'), strip=False, widget=forms.PasswordInput(
+        attrs={'autofocus': True, 'class': 'form-control'}))
+
+
+class MyPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(label=_('Old password'), strip=False, widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'autocomplete': 'current-password', 'autofocus': True}))
+    new_password1 = forms.CharField(label=_('New password'), strip=False, widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'autocomplete': 'new-password', 'autofocus': True}), help_text=password_validation.password_validators_help_text_html())
+    new_password2 = forms.CharField(label=_('Confirm New password'), strip=False, widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'autocomplete': 'new-password', 'autofocus': True}))
--- app/urls.py
+++ app/urls.py
@@ -2,18 +2,25 @@ from django.urls import path
 from app import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm, MyPasswordChangeForm
 urlpatterns = [
-    path('', views.ProductView.as_view(),name = 'home'),
-    path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('', views.ProductView.as_view(), name='home'),
+    path('product-detail/<int:pk>',
+         views.ProductDetailView.as_view(), name='product-detail'),
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
-    path('changepassword/', views.change_password, name='changepassword'),
+    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name='app/changepassword.html',
+         form_class=MyPasswordChangeForm), name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
     path('mobile/<slug:data>', views.mobile, name='mobiledata'),
-    path('login/', views.login, name='login'),
-    path('registration/', views.customerregistration, name='customerregistration'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html',
+         authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('registration/', views.CustomerResistrationView.as_view(),
+         name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
